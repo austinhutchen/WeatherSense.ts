1968413092b83dd356e847de2fbf5a3e601fafe5
Austin Hutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-05 18:09:24 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,36 @@
  #include <Adafruit_GFX.h>  
  #include <Adafruit_SSD1306.h>  
  #include <SimpleDHT.h>  
+// TECHATRONIC.COM
+  // SimpleDHT LIBRARY LINK
+  // https://github.com/winlinvip/SimpleDHT
+  // Library SPI.h
+  // https://github.com/PaulStoffregen/SPI
+  // Library Adafruit_GFX.h
+  // https://github.com/adafruit/Adafruit-GFX-Library
+  // Library Adafruit_SSD1306.h
+
+#include "SevSeg.h"
+SevSeg sevseg;
+ byte numDigits = 4;
+  byte digitPins[] = {10, 11, 12, 13};
+  byte segmentPins[] = {9, 2, 3, 5, 6, 8, 7, 4};
+
+  bool resistorsOnSegments = true;
+  bool updateWithDelaysIn = true;
+  byte hardwareConfig = COMMON_CATHODE;
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.setBrightness(90);
+ //   for DHT11,
+ //   VCC: 5V or 3V
+ //   GND: GND
+ //   DATA: 2
+ int pinDHT11 = 3;
+ SimpleDHT11 dht11(pinDHT11);
+ #define I2C_SEGMENT_DISP0 7
+ #define DHT_READ_TIME 1000
+
+
  //   for DHT11,   
  //   VCC: 5V or 3V  
  //   GND: GND  
@@ -24,10 +54,11 @@
  Adafruit_SSD1306 display(screen_width, screen_height);  
  void setup ()  
  {  
-   Serial.begin(9600);  
-   Serial.println("TEMPERATURE AND HUMIDITY");  
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  
-  display.clearDisplay();  
+     Serial.begin(9600);
+   Serial.println("TEMPERATURE AND HUMIDITY");
+    pinMode(I2C_SEGMENT_DISP0, OUTPUT);
+    digitalWrite(I2C_SEGMENT_DISP0, LOW);
+
  }  
  void loop ()  
  {  
@@ -41,18 +72,17 @@
   Serial.print((int)temperature); Serial.print(" *C, ");   
   Serial.print((int)humidity); Serial.println(" H");  
   // DHT11 sampling rate is 1HZ.  
-   display.clearDisplay();  
-   display.setTextSize(1);  
-   display.setTextColor(SSD1306_WHITE);  
-   display.setCursor(0, 0);  
-   display.print("  TEMP. & HUMIDITY");  
-   display.setCursor(0, 25);  
-   display.print(" TEMPERATURE : ");  
-   display.setCursor(90, 25);  
-   display.print((int)temperature);  
-   display.setCursor(0, 50);  
-   display.print(" HUMIDITY  : ");  
-   display.setCursor(90, 50);  
-   display.print((int)humidity);  
-   display.display();  
+ sevseg.setNumber(4999, 3);
+    sevseg.refreshDisplay();
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+   Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+   return;
+  }
+  Serial.print((int)temperature); Serial.print(" *C, ");
+  Serial.print((int)humidity); Serial.println(" H");
+  // DHT11 sampling rate is 1HZ.
+  delay(DHT_READ_TIME);
  }  
